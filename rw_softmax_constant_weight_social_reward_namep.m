0acca21de88507fc63298a7c7e1bdb26f29618f9
JenniferLcook <j.l.cook@bham.ac.uk>
2018-04-03 16:33:43 +0100

    Add  rw_softmax_constant_weight_social_reward_namep.m

---
+++ rw_softmax_constant_weight_social_reward_namep.m
@@ -0,0 +1,15 @@
+function pstruct = rw_softmax_constant_weight_social_reward_namep(pvec)
+% --------------------------------------------------------------------------------------------------
+% Copyright (C) 2012-2015 Christoph Mathys, Andreea Diaconescu TNU, UZH & ETHZ
+%
+% This file is part of the HGF toolbox, which is released under the terms of the GNU General Public
+% Licence (GPL), version 3. You can redistribute it and/or modify it under the terms of the GPL
+% (either version 3 or, at your option, any later version). For further details, see the file
+% COPYING or <http://www.gnu.org/licenses/>.
+
+pstruct = struct;
+
+pstruct.ze1 = pvec(1);
+pstruct.ze2 = pvec(2);
+
+return;
